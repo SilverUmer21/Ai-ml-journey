5f2340737a2f6badc5f3f0b6e243aceab4ac7523
SilverUmer21 <umer.ummation@gmail.com>
2026-08-24 22:43:36 +0500

    Completed lessons on test and with pytest.raises

--- Code/python_engineering/tests/test_validation.py
+++ Code/python_engineering/tests/test_validation.py
@@ -1,5 +1,10 @@
 from ml_utils import validate_binary_label
+import pytest 
 
 def test_binary_label_works() -> None:
     actual = validate_binary_label(0)
     assert actual is None
+
+def test_binary_label_raises_error() -> None:
+    with pytest.raises(ValueError, match = "Label is not"):
+        validate_binary_label(2)
